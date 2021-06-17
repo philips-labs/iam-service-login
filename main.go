772c2ad857401dbf613840a5d0cf745415200a1e
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-17 12:56:09 +0200

    Also initialize from command line

--- main.go
+++ main.go
@@ -8,13 +8,22 @@ import (
 )
 
 func main() {
+	fmt.Printf("initializing from environment (%d)\n", len(os.Args))
 	serviceID := os.Getenv("service_id")
 	privateKey := os.Getenv("private_key")
 	clientID := os.Getenv("client_id")
 	clientSecret := os.Getenv("client_secret")
 	region := os.Getenv("region")
 	environment := os.Getenv("environment")
-
+	if len(os.Args) == 7 { // Read from command line instead
+		fmt.Printf("initializing from command line arguments\n")
+		region = os.Args[1]
+		environment = os.Args[2]
+		clientID = os.Args[3]
+		clientSecret = os.Args[4]
+		serviceID = os.Args[5]
+		privateKey = os.Args[6]
+	}
 	fmt.Printf("client_id=%s\n", clientID)
 	fmt.Printf("service_id=%s\n", serviceID)
 
